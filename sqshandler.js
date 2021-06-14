646ef1ecf8ed0bd7f87b214af52614f6c07b8216
Ollie <3martynov@gmail.com>
2021-06-14 17:20:55 -0400

    initial

---
+++ sqshandler.js
@@ -0,0 +1,12 @@
+
+module.exports.sqshandler = async (event) => {
+    console.log('handler')
+    for (const record of event.Records) {
+      const messageAttributes = record.messageAttributes;
+      console.log(
+        "Message Attributte: ",
+        messageAttributes.AttributeName.stringValue
+      );
+      console.log("Message Body: ", record.body);
+    }
+  };
