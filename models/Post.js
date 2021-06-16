b79f557a30d8d9fd9e1974266c005bb48ad8d26a
Ollie <3martynov@gmail.com>
2021-06-16 17:45:11 -0400

    followup

--- models/Post.js
+++ models/Post.js
@@ -4,13 +4,28 @@ const { getConnection } = require("../helpers/databaseHelper");
 
 const conn = getConnection();
 
+const DRAW_DURATION = 24 * 60 * 60 * 1000;
+
 const postSchema = new mongoose.Schema({
+    //post type
+    type: {
+        type: String,
+        enum : [ "DEBATE", "OPINION", "VOTE", "COMMENT" ],
+        required: true
+    },
+    //parent post
+    parent: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+    },
     //address of the creator
     creator: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Account",
         required: true,
     },
+    //amount of sats locked up for the post creation by the creator
+    stake: { type: Number, required: true },
     //post title
     title: {
         type: String,
@@ -18,19 +33,25 @@ const postSchema = new mongoose.Schema({
         minlength: 3,
         maxlength: 180,
     },
-    //description of the post
-    description: {
+    //post content types
+    contentType: {
         type: String,
+        enum: [ "DEBATE","OPINION", "COMMENT" ],
         required: true,
-        minlength: 10,
+        minlength: 4,
+        maxlength: 1000,
+    },
+    //contains post data
+    content: {
+        type: String,
         maxlength: 30000,
     },
-    //post type
-    type: {
-        type: String,
-        enum : [ "DEBATE","OPINION", "COMMENT" ],
-        required: true
-    },
+    //extra fields
+    extra: {
+        type: new mongoose.Schema({
+            duration: { type: Number, default: DRAW_DURATION },
+        })
+    }
 },
 {
     timestamps: true
--- models/index.js
+++ models/index.js
@@ -3,20 +3,24 @@ const { SchemaComposer } = require('graphql-compose');
 
 module.exports.getSchema = () =>{
     const Account = require("../models/Account");
+    const Post = require("../models/Post");
     const schemaComposer = new SchemaComposer();
     const customizationOptions = {
       schemaComposer,
     };
     const AccountTC = composeMongoose(Account, customizationOptions);
+    const PostTC = composeMongoose(Post, customizationOptions);
 
     //Set GraphQL Queries from mongoose
     schemaComposer.Query.addFields({
         accountOne: AccountTC.mongooseResolvers.findOne(),
+        postOne: PostTC.mongooseResolvers.findOne(),
     });
 
     //Set GraphQL Mutations from mongoose
     schemaComposer.Mutation.addFields({
         accountCreateOne: AccountTC.mongooseResolvers.createOne(),
+        postCreateOne: PostTC.mongooseResolvers.createOne(),
     });
     return schemaComposer.buildSchema();
 }
